43ef16a7853cfbaf8f7b32a6eb31c56df5448213
Varsha Athreya <varshamathreya@gmail.com>
2024-06-26 17:43:31 -0700

    Final formating changes

--- clusters_test.py
+++ clusters_test.py
@@ -21,8 +21,8 @@ class ClusterTests(absltest.TestCase):
 
   def test_make_pandas(self):
     test_data = [['foo ', ' bar'],
-           ['1',  '2'],
-           ['3',  '4']]
+                 ['1',  '2'],
+                 ['3',  '4']]
     df = clusters.MakePandas(test_data)
     self.assertListEqual(list(df.columns), ['foo_', '_bar'])
     np.testing.assert_equal(df.values, np.array([[1, 2], [3, 4]]))
@@ -34,18 +34,16 @@ class ClusterTests(absltest.TestCase):
 
   def test_clustering(self):
     test_data = [['foo ', ' bar'],
-           [1.0,  1.0],
-           [0.9,  1.1],
-           [2.0,  2.0],
-           [1.9,  2.1],
-           ]
+                 [1.0,  1.0],
+                 [0.9,  1.1],
+                 [2.0,  2.0],
+                 [1.9,  2.1],
+                 ]
     num_clusters = 2
     df = clusters.MakePandas(test_data)
     np.random.seed(0)
     kmeans = clusters.CreateKMeans(num_clusters, df)
-    self.assertEqual(kmeans.n_clusters, num_clusters)
-
-    # , new_column_name='predictions')
+    self.assertEqual(kmeans.n_clusters, num_clusters) # , new_column_name='predictions')
     cluster_ids = clusters.KMeansPredictions(kmeans, df)
     self.assertIsInstance(cluster_ids, np.ndarray)
     # Note, clusters could be permuted, so counting classes might be better.
@@ -64,13 +62,13 @@ class ClusterTests(absltest.TestCase):
     new_kmeans, _, _ = clusters.LoadFromJson(filepath)
     cluster_centers = np.sort(new_kmeans.cluster_centers_, axis=0)
     np.testing.assert_allclose(cluster_centers, np.array([[0.95, 1.05],
-                                [1.95, 2.05]]))
+                                                          [1.95, 2.05]]))
 
   def test_classification(self):
     column_names = ['R250', 'R500', 'R1000', 'R2000', 'R3000', 'R4000', 'R6000',
-            'R8000', 'L250', 'L500', 'L1000', 'L2000', 'L3000', 'L4000',
-            'L6000', 'L8000', 'RBone500', 'RBone1000', 'RBone2000', 'RBone4000',
-            'LBone500', 'LBone1000', 'LBone2000', 'LBone4000']
+                    'R8000', 'L250', 'L500', 'L1000', 'L2000', 'L3000', 'L4000',
+                    'L6000', 'L8000', 'RBone500', 'RBone1000', 'RBone2000', 'RBone4000',
+                    'LBone500', 'LBone1000', 'LBone2000', 'LBone4000']
 
     # Make some random data and make sure we get the same results as before.
     np.random.seed(0)
@@ -96,12 +94,9 @@ class ClusterTests(absltest.TestCase):
     self.assertEqual(df_clean.shape[0], 2)
 
   def test_entropy(self):
-    self.assertAlmostEqual(
-      clusters.ComputeDiscreteEntropy([0, 0, 1, 1]), 1.0)
-    self.assertAlmostEqual(
-      clusters.ComputeDiscreteEntropy([0, 0]),     0.0)
-    self.assertAlmostEqual(
-      clusters.ComputeDiscreteEntropy([0, 1, 2, 3]), 2.0)
+    self.assertAlmostEqual(clusters.ComputeDiscreteEntropy([0, 0, 1, 1]), 1.0)
+    self.assertAlmostEqual(clusters.ComputeDiscreteEntropy([0, 0]), 0.0)
+    self.assertAlmostEqual(clusters.ComputeDiscreteEntropy([0, 1, 2, 3]), 2.0)
 
     self.assertAlmostEqual(
       clusters.ComputeContinuousEntropy(np.arange(0, 1, .001), 0, 0.125), 3.0)
